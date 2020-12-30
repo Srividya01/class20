daa477112c72bf133a83eb96cfb9fb802d15253a
Srividya01 <64191265+Srividya01@users.noreply.github.com>
2020-12-30 16:33:02 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,61 +1,34 @@
-var canvas,bg;
-var together;
-var tom, tomImg1,tomImg2;
-var jerry, jerryImg1,jerryImg2;
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
 
-function preload() {
-    bg = loadImage("images/garden.png");
-    tomImg1= loadAnimation("images/tomOne.png");
-    tomImg2=loadAnimation("images/tomTwo.png","images/tomThree.png");
-    tomImg3= loadAnimation("images/tomFour.png");
-    jerryImg1=loadAnimation("images/jerryOne.png");
-    jerryImg2= loadAnimation("images/jerryTwo.png","images/jerryThree.png");
-    jerryImg3=loadAnimation("images/jerryFour.png");
-
-}
+var engine, world;
+var box1;
+var pig1;
+var log1;
 
 function setup(){
-    canvas = createCanvas(1000,800);
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
 
-    tom = createSprite(870, 600);
-    tom.addAnimation("tomSleeping", tomImg1);
-    tom.scale = 0.2;
+    box1 = new Box(700,320,70,70);
+    box2 = new Box(900,320,70,70);
+    ground = new Ground(600,height,1200,20);
 
-    jerry = createSprite(200, 600);
-    jerry.addAnimation("jerryStanding", jerryImg1);
-    jerry.scale = 0.15;
-
+    pig1 = new Pig(810,350);
+    log1 = new Log(810,180,300,PI/2)
 }
 
-function draw() {
-
-    background(bg);
-
-    if(tom.x - jerry.x < (tom.width - jerry.width)/2)
-    { 
-        tom.velocityX=0;
-        tom.addAnimation("tomLastImage", tomImg3);
-        tom.x =300;
-        tom.scale=0.2;
-        tom.changeAnimation("tomLastImage");
-        jerry.addAnimation("jerryLastImage", jerryImg3);
-        jerry.scale=0.15;
-        jerry.changeAnimation("jerryLastImage");
-    }  
-
-    drawSprites();
-}
-
-
-function keyPressed(){
-
-    if(keyCode === LEFT_ARROW){
-        tom.velocityX = -5; 
-        tom.addAnimation("tomRunning", tomImg2);
-        tom.changeAnimation("tomRunning");
-        
-        jerry.addAnimation("jerryTeasing", jerryImg2);
-        jerry.frameDelay = 25;
-        jerry.changeAnimation("jerryTeasing");
-    }
+function draw(){
+    background(0);
+    Engine.update(engine);
+    console.log(box2.body.position.x);
+    console.log(box2.body.position.y);
+    console.log(box2.body.angle);
+    box1.display();
+    box2.display();
+    ground.display();
+    pig1.display();
+    log1.display();
 }
